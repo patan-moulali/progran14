766dc38e406911620108f5ebc139396f95b8a4c2
Patan Moulali <patan.moulali@mphasis.com>
2022-08-23 08:06:11 +0530

    Add project files.

---
+++ progran14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progran14
+{
+    internal class Program
+    {
+
+        static int count(int[] S, int m, int n)
+            {
+                if (n == 0)
+                    return 1;
+
+
+                if (n < 0)
+                    return 0;
+
+                if (m <= 0)
+                    return 0;
+
+
+                return count(S, m - 1, n) +
+                    count(S, m, n - S[m - 1]);
+            }
+            public static void Main()
+            {
+                Console.Write("n = ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Write("m = ");
+                int m = Convert.ToInt32(Console.ReadLine());
+                Console.Write("arr = ");
+                int[] arr = new int[n];
+                var stringArray = Console.ReadLine().Split(' ');
+                for (var i = 0; i < m; i++)
+                {
+                    arr[i] = int.Parse(stringArray[i]);
+                }
+                Console.WriteLine("Output = {0}", count(arr, m, n));
+            Console.ReadLine();
+            }
+    }
+}
